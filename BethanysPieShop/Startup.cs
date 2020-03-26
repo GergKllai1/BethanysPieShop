e87cd127ffb5cbf76af3bbcf47f87cd9e3fabab7
Gergo Kallai <gerg.kallai@gmail.com>
2020-03-26 09:23:13 +0100

    Basic EFCore setup. Adds nuget packages, dbcontext Adds default connection string pointing to localdb Configures in startup

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -1,6 +1,8 @@
 using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,14 +10,29 @@ namespace BethanysPieShop
 {
     public class Startup
     {
+
+        // Constructor injection to get the information from appsettings
+        public IConfiguration Configuration { get; set; }
+
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
             // The instance will be active through the request
             services.AddScoped<IPieRepository, MockPieRepository>();
             services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-            services.AddControllersWithViews();
             // Creates an instance every time the application ask for it
             // services.AddTransient() 
             // Create a single instance for the entire application and reuses that instance whenever it is called for
