db8ef784afce5154b3c6f5cf4c73d135e098faa9
Gergo Kallai <gerg.kallai@gmail.com>
2020-03-26 15:36:59 +0100

    Adds viewcomponents shoppingcart summary and category menu dropdown

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -1,6 +1,8 @@
 ﻿using BethanysPieShop.Models;
 using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,13 +19,30 @@ namespace BethanysPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(string category)
         {
-            PieListViewModel pieListViewModel = new PieListViewModel();
-            pieListViewModel.Pies = _pieRepository.AllPies;
-            pieListViewModel.CurrentCategory = "Cheese cakes";
+            IEnumerable<Pie> pies;
+            string currentCategory;
 
-            return View(pieListViewModel);
+            if(string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies
+                    .Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories
+                    .FirstOrDefault(c => c.CategoryName == category).CategoryName;
+            };
+
+            return View(new PieListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
 
         // The name of the parameter has to be the same as defined in the endPoints pattern in 
